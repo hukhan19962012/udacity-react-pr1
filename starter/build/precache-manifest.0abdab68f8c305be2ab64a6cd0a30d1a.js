3870347ac70f64e692565bb2862edb04d0751a2c
CUONGLT\cuong <cuonglt300499@gmail.com>
2024-03-14 23:01:20 +0700

    first commit

---
+++ starter/build/precache-manifest.0abdab68f8c305be2ab64a6cd0a30d1a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "364e252f81dcfa749cdb",
+    "url": "/static/css/main.dba0ba3d.chunk.css"
+  },
+  {
+    "revision": "364e252f81dcfa749cdb",
+    "url": "/static/js/main.364e252f.chunk.js"
+  },
+  {
+    "revision": "ef25d18222f7f70e6de8",
+    "url": "/static/js/1.ef25d182.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "55cd81a35467b5fa38800d07c341eb76",
+    "url": "/index.html"
+  }
+];
