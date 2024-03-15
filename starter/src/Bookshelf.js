50ce6920f235810b696eceb3026181510d2474ea
CUONGLT\cuong <cuonglt300499@gmail.com>
2024-03-15 23:25:50 +0700

    update app

--- starter/src/Bookshelf.js
+++ starter/src/Bookshelf.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {Book} from './Book';
 
-export const Bookshelf = (props) => {
-  const { shelf, books, onMove } = props;
-  const onSelfBooks = books.filter(book => book.shelf === shelf.key);
+export const Bookshelf = ({ shelf, books, onMove }) => {
+  const onSelfBooks = books.filter(x => x.shelf === shelf.key);
   return (
     <div className="book-shelf">
       <h2 className="book-shelf-title">{shelf.name}</h2>
       <div className="book-shelf-books">
-        <ol className="books-grid">
+        <ol className="books-grid-view">
           {onSelfBooks.map(x => (
             <Book key={x.id} book={x} shelf={shelf.key} onMove={onMove} />
           ))}
